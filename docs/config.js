912055e547374b7a294307f8aa41176f7d01fd08
Jiankun Zhang <phycholosogy@qq.com>
2026-06-26 00:41:43 +0800

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,4 @@
 window.__APP_CONFIG__ = {
   API_BASE: "https://classroom-craft-sacrifice-modelling.trycloudflare.com",
+  USER_LLM_API_BASE: "https://burlington-flow-comes-touring.trycloudflare.com",
 };
